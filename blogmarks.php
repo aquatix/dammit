c70575afcba346b5c593c74c6368cb0e82925966
Michiel Scholten <michiel@aquariusoft.org>
2008-08-19 09:51:34 +0000

    Added gpl header for blogmarks.php, fixed pointer to the gpl in search.php and index.php (don't use the physical address, but their website)
    --HG--
    extra : convert_revision : svn%3A0c6641da-d714-0410-886f-91f04f3f27cb/trunk%4076

--- blogmarks.php
+++ blogmarks.php
@@ -1,6 +1,25 @@
 <?php
-$lastmodified = '2006-10-22';
-$page_version = '0.5.02';
+/*
+ * $Id$
+ *
+ * Copyright 2003-2008 mbscholt at aquariusoft.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$lastmodified = '2008-08-19';
+$page_version = '0.5.03';
 $dateofcreation = '2003-12-21';
 
 $section_name = 'blogmarks';
