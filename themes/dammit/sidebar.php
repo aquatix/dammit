76e95208de5fabc98aec40c881ea6d051fb9904c
Michiel Scholten <michiel@aquariusoft.org>
2013-04-13 12:01:09 +0200

    Glyphs in the navigation
    --HG--
    branch : v0.8_html5

--- themes/dammit/sidebar.php
+++ themes/dammit/sidebar.php
@@ -5,18 +5,19 @@
 		
 		<nav>
 			<ol>
-				<li><a href="<?php echo $skel['base_uri'];?>" accesskey="h" title="Home">home</a></li>
-				<li><a href="<?php echo $skel['base_uri'];?>p/archive" accesskey="a" title="View all post titles in the archive">archive</a></li>
-				<li><a href="<?php echo $skel['base_uri'];?>m" accesskey="m" title="View interesting links">blogmarks</a></li>
-				<li><a href="<?php echo $skel['base_uri'];?>p/books" accesskey="b" title="View interesting links">books</a></li>
-				<li><a href="<?php echo $skel['base_uri'];?>p/kudos" accesskey="k" title="View a list of sites that deserve kudos">kudos</a></li>
-				<li><a href="<?php echo $skel['base_uri'];?>p/about" accesskey="?" title="Information about the author">about</a></li>
+				<li><a href="<?php echo $skel['base_uri'];?>" accesskey="h" title="Home"><span class="glyph">&#8962;</span> home</a></li>
+				<li><a href="<?php echo $skel['base_uri'];?>p/archive" accesskey="a" title="View all post titles in the archive"><span class="glyph">&#128230;</span> archive</a></li>
+				<li><a href="<?php echo $skel['base_uri'];?>m" accesskey="m" title="View interesting links"><span class="glyph">&#128209;</span> blogmarks</a></li>
+				<li><a href="<?php echo $skel['base_uri'];?>p/books" accesskey="b" title="The list of books I've read or are going to consume"><span class="glyph">&#128213;</span> books</a></li>
+				<li><a href="<?php echo $skel['base_uri'];?>p/kudos" accesskey="k" title="View a list of sites that deserve kudos"><span class="glyph">&#128077;</span> kudos</a></li>
+				<li><a href="<?php echo $skel['base_uri'];?>p/about" accesskey="?" title="Information about the author"><span class="glyph">&#59170;</span> about</a></li>
 			</ol>
 		</nav>
 
 		<div class="pageversion">v<?php echo $skel['page_version']; ?></div>
 		
 		<?php if (!isset($searchkey)) { $searchkey = ''; } ?>
+		<?php /*<form style="margin-top: 1em;" action="<?php echo $skel['base_uri'];?>search" method="post"><div><input type="text" class="searchfield" name="searchkey" size="12" maxlength="250" value="<?php echo $searchkey;?>" /><input name="searchbtn" value="&#128269;" type="submit" class="searchbtn" /></div></form>*/ ?>
 		<form style="margin-top: 1em;" action="<?php echo $skel['base_uri'];?>search" method="post"><div><input type="text" class="searchfield" name="searchkey" size="12" maxlength="250" value="<?php echo $searchkey;?>" /><input name="searchbtn" value="Find" type="submit" /></div></form>
 		
 		<h2>Distracted by</h2>
